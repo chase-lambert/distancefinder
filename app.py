b004aab478050d79ec624aef2cf0a874bc3193ca
Chase Lambert <chaselambert@gmail.com>
2020-12-23 09:41:29 -0500

    hard code destinations

--- app.py
+++ app.py
@@ -31,26 +31,47 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
     locations = [{'location': 'Yigo, Guam', 'lat': 13.535204499999999, 'long': 144.89715694673106}, {'location': 'Medellin, Colombia', 'lat': 6.2443382, 'long': -75.573553}, {'location': 'Edinburgh, Scotland', 'lat': 55.9533456, 'long': -3.1883749}, {'location': 'King, NC', 'lat': 36.275245999999996, 'long': -80.36568613272892}, {'location': 'Redgranite, WI', 'lat': 44.0419238, 'long': -89.0984504}, {'location': 'Marana, AZ', 'lat': 32.4446988, 'long': -111.2157091}, {'location': 'Dudley, NC', 'lat': 35.2673857, 'long': -78.0374891}, {'location': 'Fairfield, CA', 'lat': 38.2493581, 'long': -122.039966}]
 
     destinations = [{'location': 'Fort Payne, Alabama', 'lat': 34.4442547, 'long': -85.7196893}, {'location': 'Hot Springs, Arkansas', 'lat': 34.5038393, 'long': -93.0552437}, {'location': 'Canyon, Texas', 'lat': 34.99253385, 'long': -101.92788331921604}, {'location': 'Pena Blanca, New Mexico', 'lat': 35.574754999999996, 'long': -106.33723818363845}, {'location': 'Williams, AZ', 'lat': 35.2503394, 'long': -112.1869481}, {'location': 'Springfield, Utah', 'lat': 37.1908427, 'long': -93.2932611}, {'location': 'Torrey, Utah', 'lat': 38.2997368, 'long': -111.4204705}, {'location': 'Moab, Utah', 'lat': 38.5738096, 'long': -109.5462146}, {'location': 'Ashton, Idaho', 'lat': 44.071581, 'long': -111.448288}, {'location': 'Browning, Montana', 'lat': 48.557743, 'long': -113.0172586}, {'location': 'Custer, South Dakota', 'lat': 43.6726477, 'long': -103.5101597}, {'location': 'Redgranite, WI', 'lat': 44.0419238, 'long': -89.0984504}, {'location': 'Marana, AZ', 'lat': 32.4446988, 'long': -111.2157091}, {'location': 'King, NC', 'lat': 36.275245999999996, 'long': -80.36568613272892}]
 
-    for location in locations:
-        lat = location["lat"]
-        long = location["long"]
-        distance = round(haversine(current_long, current_lat, long, lat))
-        distance = {"distance": distance}
-        location.update(distance)
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+        
+        location = request.form.get("location")
+        for d in destinations:
+            if d['location'] = location
+                current_destination = d
 
-    return render_template(
-        "index.html",
-        locations=locations,
-        current_lat=current_lat,
-        current_long=current_long,
-        ip=ip,
-    )
+        current_lat = current_destination["lat"]
+        current_long = current_destination["long"]
+
+        for location in locations:
+            lat = location["lat"]
+            long = location["long"]
+            distance = round(haversine(current_long, current_lat, long, lat))
+            distance = {"distance": distance}
+            location.update(distance)
+
+        return redirect("/")
+
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        for location in locations:
+            lat = location["lat"]
+            long = location["long"]
+            distance = round(haversine(current_long, current_lat, long, lat))
+            distance = {"distance": distance}
+            location.update(distance)
+
+        return render_template(
+            "index.html",
+            locations=locations
+            destinations=destinations,
+        )
 
 
 def haversine(lon1, lat1, lon2, lat2):
